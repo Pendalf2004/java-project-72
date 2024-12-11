df8bfef0494c6b7456235dde901d4721f65999c9
John Snow <qaz20042004@gmail.com>
2024-12-11 19:24:35 +0300

    rollback

--- app/src/main/java/hexlet/code/repository/CheckRepository.java
+++ app/src/main/java/hexlet/code/repository/CheckRepository.java
@@ -15,21 +15,20 @@ public class CheckRepository extends BaseDB {
                 "INSERT INTO UrlCheck (urlId, statusCode, title, h1, description) VALUES (?, ?, ?, ?, ?)";
         try (var conn = dataConfig.getConnection();
              var preparedStatement = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
-            preparedStatement.setLong(1, check.getUrlId());
-            preparedStatement.setInt(2, check.getStatusCode());
-            preparedStatement.setString(3, check.getTitle());
-            preparedStatement.setString(4, check.getH1());
-            preparedStatement.setString(5, check.getDescription());
+            preparedStatement.setLong   (1, check.getUrlId());
+            preparedStatement.setInt    (2, check.getStatusCode());
+            preparedStatement.setString (3, check.getTitle());
+            preparedStatement.setString (4, check.getH1());
+            preparedStatement.setString (5, check.getDescription());
             preparedStatement.executeUpdate();
             var generatedKeys = preparedStatement.getGeneratedKeys();
             if (generatedKeys.next()) {
                 check.setId(generatedKeys.getLong(1));
-                //if (dataConfig.getJdbcUrl().startsWith("jdbc:h2")) {        //  тут костыль, потому, что h2 возвращает
+                if (dataConfig.getJdbcUrl().startsWith("jdbc:h2")) {        //  тут костыль, потому, что h2 возвращает
                     check.setCreatedAt(generatedKeys.getTimestamp(2));   //  в generatedKeys только сгенерированные
-                //} else {                                                    //  поля, а postgre - все
-                //    check.setCreatedAt(generatedKeys.getTimestamp(7));
-                //}
-
+                } else {                                                    //  поля, а postgre - все
+                    check.setCreatedAt(generatedKeys.getTimestamp(7));
+                }
             } else {
                 throw new SQLException("Database have not returned an id or createdAt after saving an entity");
             }
@@ -38,25 +37,24 @@ public class CheckRepository extends BaseDB {
     }
 
     public static List<CheckModel> findAllByUrlId(Long urlId) throws SQLException {
-        String query = "SELECT * FROM UrlCheck "
-                + "WHERE urlId = " + urlId
-                + "ORDER BY id DESC;";
+        String query =  "SELECT * FROM UrlCheck "
+                +       "WHERE urlId = " + urlId
+                +       "ORDER BY id DESC;";
         try (var conn = dataConfig.getConnection();
             var preparedStatement = conn.prepareStatement(query)) {
             var checksList = preparedStatement.executeQuery();
             var result = new ArrayList<CheckModel>();
             while (checksList.next()) {
-                var check = new CheckModel(checksList.getLong("urlId"));
-                check.setId(checksList.getLong("id"));
-                check.setTitle(checksList.getString("title"));
-                check.setH1(checksList.getString("h1"));
-                check.setDescription(checksList.getString("description"));
-                check.setStatusCode(checksList.getInt("statusCode"));
-                check.setCreatedAt(checksList.getTimestamp("created_at"));
+                var check = new CheckModel(checksList.getLong   ("urlId"));
+                check.setId(checksList.getLong                  ("id"));
+                check.setTitle(checksList.getString             ("title"));
+                check.setH1(checksList.getString                ("h1"));
+                check.setDescription(checksList.getString       ("description"));
+                check.setStatusCode(checksList.getInt           ("statusCode"));
+                check.setCreatedAt(checksList.getTimestamp      ("created_at"));
                 result.add(check);
             }
             return result;
         }
     }
-
 }
