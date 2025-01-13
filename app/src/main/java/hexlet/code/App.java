c1857fb3f555d68fc16dd7d9a8dbc9f1634af631
John Snow <qaz20042004@gmail.com>
2025-01-13 10:43:03 +0300

    dup bd init from DBUtils to App

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -2,6 +2,9 @@ package hexlet.code;
 
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
+import gg.jte.ContentType;
+import gg.jte.TemplateEngine;
+import gg.jte.resolve.ResourceCodeResolver;
 import hexlet.code.controller.ChecksController;
 import hexlet.code.controller.RootController;
 import hexlet.code.controller.UrlController;
@@ -10,23 +13,63 @@ import io.javalin.Javalin;
 import io.javalin.rendering.template.JavalinJte;
 import hexlet.code.utils.DBUtils;
 import hexlet.code.utils.NamedRoutes;
+
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.sql.SQLException;
+import java.util.stream.Collectors;
 
 
 public class App {
+    public static HikariDataSource dataSource;
+
     public static void main(String[] args) throws SQLException, IOException {
+        //DBUtils.createDB();
+        var page = getApp();
+        page.start(DBUtils.getPort());
+    }
+
+    public static int getPort() {
+        String port = System.getenv().getOrDefault("PORT", "7070");
+        return Integer.parseInt(port);
+    }
+
+    public static TemplateEngine createTemplateEngine() {
+        ClassLoader classLoader = DBUtils.class.getClassLoader();
+        ResourceCodeResolver codeResolver = new ResourceCodeResolver("templates/jte", classLoader);
+        TemplateEngine templateEngine = TemplateEngine.create(codeResolver, ContentType.Html);
+        return templateEngine;
+    }
+
+    public static String getDbConfig() {
+        return System.getenv().getOrDefault(
+                "JDBC_DATABASE_URL",
+                //"jdbc:postgresql://localhost/postgres?password=password&user=postgres");
+                "jdbc:h2:mem:project;DB_CLOSE_DELAY=-1;");
+    }
+
+    public static void createDB() throws SQLException, IOException {
         var hikariConfig = new HikariConfig();
-        HikariDataSource dataSource = new HikariDataSource(hikariConfig);
-        hikariConfig.setJdbcUrl(DBUtils.getDbConfig());
+        hikariConfig.setJdbcUrl(getDbConfig());
         if (hikariConfig.getJdbcUrl().startsWith("jdbc:postgresql")) { //почему-то для postgre не
             // подгружаются драйвера автоматически
             hikariConfig.setDriverClassName(org.postgresql.Driver.class.getName());
         }
+        dataSource = new HikariDataSource(hikariConfig);
         BaseDB.dataSource = dataSource;
-        DBUtils.createDB();
-        var page = getApp();
-        page.start(DBUtils.getPort());
+
+        String query = "";
+        try (var queryFile = ClassLoader.getSystemClassLoader().getResourceAsStream("schema.sql")) {
+            query = new BufferedReader(new InputStreamReader(queryFile))
+                    .lines()
+                    .collect(Collectors.joining("\n"));
+        } finally {
+            try (var connection = dataSource.getConnection();
+                 var statement = connection.createStatement()) {
+                statement.execute(query);
+            }
+        }
     }
 
     public static Javalin getApp() {
