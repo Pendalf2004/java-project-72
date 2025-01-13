64420add6562e9844814f26b48b65f1d055038dc
John Snow <qaz20042004@gmail.com>
2025-01-13 10:33:12 +0300

    added hikariConfig init onto App class

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,8 +1,11 @@
 package hexlet.code;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
 import hexlet.code.controller.ChecksController;
 import hexlet.code.controller.RootController;
 import hexlet.code.controller.UrlController;
+import hexlet.code.repository.BaseDB;
 import io.javalin.Javalin;
 import io.javalin.rendering.template.JavalinJte;
 import hexlet.code.utils.DBUtils;
@@ -13,6 +16,14 @@ import java.sql.SQLException;
 
 public class App {
     public static void main(String[] args) throws SQLException, IOException {
+        var hikariConfig = new HikariConfig();
+        HikariDataSource dataSource = new HikariDataSource(hikariConfig);
+        hikariConfig.setJdbcUrl(DBUtils.getDbConfig());
+        if (hikariConfig.getJdbcUrl().startsWith("jdbc:postgresql")) { //почему-то для postgre не
+            // подгружаются драйвера автоматически
+            hikariConfig.setDriverClassName(org.postgresql.Driver.class.getName());
+        }
+        BaseDB.dataSource = dataSource;
         DBUtils.createDB();
         var page = getApp();
         page.start(DBUtils.getPort());
